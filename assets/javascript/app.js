9b7cfa0ed25302e6494c1cc2636236479eb2aae9
Jixu NI <40869621+jixuni@users.noreply.github.com>
2018-10-27 21:12:25 -0400

    fix some javascript bugs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,60 +4,88 @@ var trivia = [];
 var choices = [];
 var correctAnswer;
 var sec = 30;
+var winCounter;
+var loseCounter;
 rand = Math.floor(Math.random() * 10);
 var timer = setInterval(function(){
-    $("#timer").text(sec--);
+    $("#timer").text("Time Remaining: " + sec--);
     if(sec == -1){
+        $("#button-container").html("TIMES up!");
         //alert("Time is up!")
     }
 }, 1000);
 
 
 
+
+gameOn(); // start game
+
+function gameOn(){
 $.ajax({
     url: queryURL,
     method: "GET",
 }).then(function(response){
     correctAnswer = response.results[rand].correct_answer;
-    choices.push(response.results[rand].correct_answer);
-    for(var i = 0; i < 3; i++){
-        choices.push(response.results[rand].incorrect_answers[i]);
+    choices.push(response.results[rand].correct_answer); // 1 correct answer
+    for(var i = 0; i < 3 ; i++){
+        choices.push(response.results[rand].incorrect_answers[i]); // 3 wrong answer in the JSON object
     }
-    trivia.push(response.results[rand].question);
+    trivia.push(response.results[rand].question); // pushing question to array
     $("#trivia").html(trivia[0]);  
-    console.log(trivia, rand)
     
-    for(var i = 0; i < choices.length; i++ ){
-        var htmlButton = $("#answer" + i);
-        htmlButton.html(choices[i]);
-        htmlButton.attr("value", choices[i]);
+    shuffleAnswer = shuffle(choices); 
+    for(var i = 0; i < shuffleAnswer.length; i++ ){
+        var htmlButton = $("#btn" + i);
+        htmlButton.html(shuffleAnswer[i]);
+        htmlButton.attr("value", shuffleAnswer[i]);
     }
-    
-    
-    //choices.push(response.results[rand].incorrect_answers);
-    // for(i = 0; i < 10; i++){
-    //         trivia.push(response.results[i].question);
-    //         console.log(response.results[i].question);
-    //     }
-     console.log(response);
-     console.log(choices);
-    console.log(rand);
-    console.log(response.results[rand].correct_answer);
-    console.log(correctAnswer);
-   //var questionDiv = $("<div class='questions'");
-    
-    //var htmlQuestion = $("<p>").text(trivia);
-    
-    //questionDiv.append(htmlQuestion);
 })
+}
 
 
-$(this).on("click", function(){
-    var currentAnswer = $(this).attr("value") 
+
+
+$("button").on("click", function(){
+    var currentAnswer = $(this).attr("value");
     console.log(currentAnswer);
     if(currentAnswer === correctAnswer){
-        console.log("Correct")
+        winCounter++;
+        $("#button-container").html("Correct!");
+        setTimeout(gameOn, 2000);
+        console.log("Correct");
     }else{
-        console.log("wrong!")
+        loseCounter--;
+        $("#button-container").html("WRONG!");
+        setTimeout(gameOn, 2000);
+        console.log("wrong!");
     }
 })
+
+
+// function called fisher-yates shuffle found online
+function shuffle(array) {
+    var m = array.length, t, i;
+  
+    // While there remain elements to shuffle…
+    while (m) {
+  
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+  
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+  
+    return array;
+}
+
+
+function winCondtion (){
+    if(winCounter === 5){
+        alert("Congrats you win");
+    }else if(loseCounter === 3){
+        alert("Game over");
+    }
+}
